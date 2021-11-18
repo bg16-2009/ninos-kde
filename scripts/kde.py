4f3aabbb06853dadf92d86a68f8028206d00d9a1
Bogdan Gabor <bogdan.gabor.cj@gmail.com>
2021-11-18 14:58:28 +0000

    update

--- scripts/kde.py
+++ scripts/kde.py
@@ -1,4 +1,9 @@
 import pacman
+import os
 
 def install_kde():
     print("Installing kde.......")
+    pacman.install(["xorg", "plasma", "plasma-wayland-session", "kde-applications", "networkmanager"])
+    print("Configuring kde........")
+    os.system("systemctl enable sddm.service")
+    os.system("systemctl enable NetworkManager.service")
